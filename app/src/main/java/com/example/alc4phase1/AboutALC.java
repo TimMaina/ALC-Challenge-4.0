dcb59ea9982522c2462096e13787d4f51cbd1754
TIM <timmaina64@gmail.com>
2019-07-15 18:01:11 +0300

    1st repository trial

--- app/src/main/java/com/example/alc4phase1/AboutALC.java
+++ app/src/main/java/com/example/alc4phase1/AboutALC.java
@@ -1,29 +1,42 @@
 package com.example.alc4phase1;
 
 import android.annotation.SuppressLint;
+import android.net.http.SslError;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.SslErrorHandler;
 import android.webkit.WebView;
-
+import android.webkit.WebViewClient;
 
 public class AboutALC extends AppCompatActivity {
 
-    private WebView webView;
-
-
+    WebView webView;
 
     @SuppressLint(value = "RestrictedApi")
     @Override
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_alc);
         getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
 
     webView = (WebView) findViewById(R.id.webView);
-    webView.loadUrl("https://www.andela.com/alc/");
-
+    webView.setWebViewClient(new MyBrowser());
+    webView.getSettings().setJavaScriptEnabled(true);
+    webView.loadUrl("https://andela.com/alc/");
     }
-}
+    private class MyBrowser extends WebViewClient{
+
+
+     @Override
+        public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+            handler.proceed();
+        }
+    }
+    }
+
+
+
 
 
 
--- app/src/main/java/com/example/alc4phase1/MainActivity.java
+++ app/src/main/java/com/example/alc4phase1/MainActivity.java
@@ -1,4 +1,5 @@
 package com.example.alc4phase1;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,11 +9,15 @@ import android.widget.Button;
 public class MainActivity extends Activity implements View.OnClickListener {
     private Button button;
     private Button button1;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Button button = findViewById(R.id.button);
+
 
         Button button1 = findViewById(R.id.button1);
         button.setOnClickListener(new View.OnClickListener() {
@@ -31,6 +36,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
 }
     @Override
     public void onClick(View v) {
+
+
 
     }
 }
